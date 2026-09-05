a2472d0324340a3b042a3dec8bed7371a82f6c99
kerulsukee <skhairulaji@gmail.com>
2026-09-05 15:25:56 +0700

    testing

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,8 +3,11 @@ const app = require("../api/index");
 
 describe("GET /", () => {
   test("should return message", async () => {
-    const response = await request(app);
+    const response = await request(app).get("/");
 
     expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      message: "CI/CD PROJECT",
+    });
   });
 });
