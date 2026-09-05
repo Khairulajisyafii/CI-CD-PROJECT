c95a8357e7a4bb3fe6407100e11877eb6f1ab7e3
kerulsukee <skhairulaji@gmail.com>
2026-09-05 14:35:07 +0700

    configure for vercel

--- api/index.js
+++ api/index.js
@@ -2,8 +2,30 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("vercel teset succeed");
+  res.json({
+    message: "Apple Academy CI/CD Project",
+  });
 });
+
+app.get("/tasks", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      title: "Learn CI/CD",
+    },
+  ]);
+});
+
+// Jalankan server hanya ketika dijalankan secara lokal
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
 
 module.exports = app;
